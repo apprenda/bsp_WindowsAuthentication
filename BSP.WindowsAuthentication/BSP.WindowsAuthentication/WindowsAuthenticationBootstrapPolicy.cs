9195eb810c811d7a372d070fe15ebde7a102d741
Patriek van Dorp <Patriek van Dorp>
2016-10-19 11:26:18 +0200

    Bug fix

--- BSP.WindowsAuthentication/BSP.WindowsAuthentication/WindowsAuthenticationBootstrapPolicy.cs
+++ BSP.WindowsAuthentication/BSP.WindowsAuthentication/WindowsAuthenticationBootstrapPolicy.cs
@@ -10,7 +10,9 @@ namespace BSP.WindowsAuthenticationP
         public override BootstrappingResult Bootstrap(BootstrappingRequest bootstrappingRequest)
         {
             //Only modify .NET Websites Components that do not belong to the Apprenda Team
-            if (bootstrappingRequest.ComponentType == ComponentType.AspNet &&
+            if ((bootstrappingRequest.ComponentType == ComponentType.AspNet | 
+                bootstrappingRequest.ComponentType == ComponentType.PublicAspNet | 
+                bootstrappingRequest.ComponentType == ComponentType.WcfService) &&
                 !bootstrappingRequest.DevelopmentTeamAlias.Equals("apprenda", StringComparison.InvariantCultureIgnoreCase))
             {
                 return ModifyConfigFiles(bootstrappingRequest);
@@ -47,8 +49,8 @@ namespace BSP.WindowsAuthenticationP
                 //Traverse the web.config file and find the required section
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.Load(filePath);
-
-                XmlElement systemWebServer = (XmlElement)xmlDoc.SelectSingleNode("//system.webServer");
+                
+                XmlNode systemWebServer = xmlDoc.SelectSingleNode("//system.webServer");
 
                 //If there is no custom errors setting defined, add it and save the modifed configuration file
                 /* <security>
@@ -58,32 +60,38 @@ namespace BSP.WindowsAuthenticationP
                      </authentication>
                   </security>
                 */
+
+                XmlNode security = xmlDoc.CreateNode(XmlNodeType.Element, "security", null);
+                XmlNode authentication = xmlDoc.CreateNode(XmlNodeType.Element, "authentication", null);
+                XmlNode anonymousAuthentication = xmlDoc.CreateNode(XmlNodeType.Element, "anonymousAuthentication", null);
+                XmlNode windowsAuthentication = xmlDoc.CreateNode(XmlNodeType.Element, "windowsAuthentication", null);
+                XmlAttribute anonymousAuthenticationEnabled = xmlDoc.CreateAttribute("enabled");
+                XmlAttribute windowsAuthenticationEnabled = xmlDoc.CreateAttribute("enabled");
+                anonymousAuthenticationEnabled.Value = "false";
+                windowsAuthenticationEnabled.Value = "true";
+                anonymousAuthentication.Attributes.Append(anonymousAuthenticationEnabled);
+                windowsAuthentication.Attributes.Append(windowsAuthenticationEnabled);
+                authentication.AppendChild(anonymousAuthentication);
+                authentication.AppendChild(windowsAuthentication);
+                security.AppendChild(authentication);
+
                 if (null == systemWebServer)
                 {
-                    XmlNode security = xmlDoc.CreateNode(XmlNodeType.Element, "security", null);
-                    XmlNode authentication = xmlDoc.CreateNode(XmlNodeType.Element, "authentication", null);
-                    XmlNode anonymousAuthentication = xmlDoc.CreateNode(XmlNodeType.Element, "anonymousAuthentication", null);
-                    XmlNode windowsAuthentication = xmlDoc.CreateNode(XmlNodeType.Element, "windowsAuthentication", null);
-                    XmlAttribute anonymousAuthenticationEnabled = xmlDoc.CreateAttribute("enabled");
-                    XmlAttribute windowsAuthenticationEnabled = xmlDoc.CreateAttribute("enabled");
-                    anonymousAuthenticationEnabled.Value = "false";
-                    windowsAuthenticationEnabled.Value = "true";
-                    anonymousAuthentication.Attributes.Append(anonymousAuthenticationEnabled);
-                    windowsAuthentication.Attributes.Append(windowsAuthenticationEnabled);
-                    authentication.AppendChild(anonymousAuthentication);
-                    authentication.AppendChild(windowsAuthentication);
-                    security.AppendChild(authentication);
+                    systemWebServer = xmlDoc.CreateNode(XmlNodeType.Element, "system.webServer", null);
                     systemWebServer.AppendChild(security);
+
+                    XmlNode configuration = xmlDoc.SelectSingleNode("//configuration");
+                    configuration.AppendChild(systemWebServer);
                     xmlDoc.Save(filePath);
                     return BootstrappingResult.Success();
                 }
                 //If there is a custom errors setting configuration, overwrite it and set it to off
-                //else if (null != customErrors && appsettingsNode != null)
-                //{
-                //    customErrors.Attributes["mode"].Value = "off";
-                //    xmlDoc.Save(filePath);
-                //    return BootstrappingResult.Success();
-                //}
+                else if (null != systemWebServer)
+                {
+                    systemWebServer.AppendChild(security);
+                    xmlDoc.Save(filePath);
+                    return BootstrappingResult.Success();
+                }
                 return BootstrappingResult.Success();
             }
             catch (Exception ex)
